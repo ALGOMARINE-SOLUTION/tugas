280f4be4d329c07243579dc930e20a2bfb00c66f
ALGOMARINE <algomarine.undip@gmail.com>
2021-02-18 15:16:58 +0700

    New commie
    Perubahan kedua selanjutnya

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask
+from routing.home import app as home
 
 app = Flask(__name__)
-
-@app.route('/')
-def halamanUtama():
-    return "Selamat Datang di Halaman Utama"
+app.register_blueprint(home)
 
 if __name__ == '__main__':
     app.run()
